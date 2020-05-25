6d2d97efde2c8d891a3ee70ec5d289bd3867b252
Pratik Kohli <pratik.h.kohli@gmail.com>
2020-05-25 23:55:25 +0100

    added basename to router as needed for serving that app from a subfolder

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,11 @@ import Dashboard from '../screens/Dashboard';
 class Routes extends Component {
     render() {
         return (
-            <Router>
+            /**
+             * basename is needed if deploying to a non-root folder as exact route / will expect
+             * the root folder and so will not work without basname if on a subfolder.
+             */
+            <Router basename={'/react-components/'}>
                 <Switch>
                     <Route exact path='/' component={Dashboard} />
                 </Switch>
