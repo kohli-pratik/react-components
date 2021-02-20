a736f2e1ee619e4a1e374c25402522db8735b164
Pratik Kohli <pratik.h.kohli@gmail.com>
2021-02-20 19:22:48 +0000

    updated eslint rules. added config for tests. added pre-commit checks

---
+++ src/screens/Dashboard/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Dashboard from '.';
+import BottomNavigation from '../../features/BottomNavigation';
+import BackToTop from '../../features/BackToTop';
+import ToggleFeatures from '../../features/ToggleFeatures';
+
+Enzyme.configure({ adapter: new Adapter() });
+let wrapper;
+// let wrapperInstance;
+
+describe('Testing dashboard screen component', () => {
+    beforeEach(() => {
+        wrapper = shallow(<Dashboard />);
+        // wrapperInstance = wrapper.instance();
+    });
+
+    test('Should contain BottomNavigation, BackToTop and ToggleFeatures on load', () => {
+        const bottomNavigation = wrapper.find(BottomNavigation);
+        const backToTop = wrapper.find(BackToTop);
+        const toggleFeatures = wrapper.find(ToggleFeatures);
+        expect(bottomNavigation.exists()).toBe(true);
+        expect(backToTop.exists()).toBe(true);
+        expect(toggleFeatures.exists()).toBe(true);
+    });
+});
